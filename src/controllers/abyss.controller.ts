6bdab8caae12bb142f04434ad0e812f0292f52c8
Nilesh Viduranga <nnviduranga@gmail.com>
2025-04-02 14:59:27 +0530

    Refactor abyss data handling by introducing parsing utilities for characters and parties

--- src/controllers/abyss.controller.ts
+++ src/controllers/abyss.controller.ts
@@ -2,6 +2,7 @@ import {
   fetchAbyssBlessingInfo,
   fetchAbyssInfo
 } from "../services/system.service";
+import { parseCharacterData, parsePartyData } from "../utils/abyssDataParser";
 
 export const getAbyssData = async () => {
   try {
@@ -12,79 +13,18 @@ export const getAbyssData = async () => {
       meta: abyssParsed.meta,
       data: {
         schedule: abyssParsed.data.schedule,
-        sample_size: abyssParsed.data.sample_size,
-        sample_size_x_a: abyssParsed.data.sample_size_x_a,
-        sample_size_x_b: abyssParsed.data.sample_size_x_b,
-        sample_collection_progress: abyssParsed.data.sample_collection_progress,
-        sample_countries: abyssParsed.data.sample_countries,
+        sampleSize: abyssParsed.data.sample_size,
+        sampleSize_x_a: abyssParsed.data.sample_size_x_a,
+        sampleSize_x_b: abyssParsed.data.sample_size_x_b,
+        sampleCollectionProgress: abyssParsed.data.sample_collection_progress,
+        sampleCountries: abyssParsed.data.sample_countries,
         threshold: abyssParsed.data.threshold
       },
-      characters: Object.keys(abyssParsed.data.character).map((key) => {
-        const character = abyssParsed.data.character[key];
-        return {
-          id: key,
-          use_rate: character.use_rate,
-          own_rate: character.own_rate,
-          use_by_own_rate: character.use_by_own_rate,
-          weapons: character.weapons.map((weapon) => ({
-            id: weapon.id,
-            value: weapon.value
-          })),
-          artifacts: character.artifacts.map((artifact) => ({
-            set: artifact.set,
-            value: artifact.value
-          })),
-          constellations: character.constellations.map((constellation) => ({
-            id: constellation.id,
-            value: constellation.value
-          }))
-          //   relation_overall: character.relation_overall.map((relation) => ({
-          //     id: relation.id,
-          //     value: relation.value,
-          //   })),
-          //   relation_by_constellation: Object.keys(
-          //     character.relation_by_constellation
-          //   ).map((constellationKey) => ({
-          //     constellation: constellationKey,
-          //     relations: Object.keys(
-          //       character.relation_by_constellation[constellationKey]
-          //     ).map((relationKey) => ({
-          //       id: relationKey,
-          //       value:
-          //         character.relation_by_constellation[constellationKey][
-          //           relationKey
-          //         ],
-          //     })),
-          //   })),
-          //   best_damages: character.best_damages.map((damage) => ({
-          //     p: damage.p,
-          //     value: damage.value,
-          //   })),
-          //   builds: character.builds.map((build) => ({
-          //     id: build.id,
-          //     artifact_set: build.artifact_set,
-          //     artifact_prop_main_ids: build.artifact_prop_main_ids,
-          //     weapons: build.weapons.map((weapon) => ({
-          //       id: weapon.id,
-          //       value: weapon.value,
-          //     })),
-          //     constellations: build.constellations.map((constellation) => ({
-          //       id: constellation.id,
-          //       value: constellation.value,
-          //     })),
-          //     sample_size: build.sample_size,
-          //   })),
-        };
-      }),
-      parties: Object.keys(abyssParsed.data.party).map((key) => {
-        const party = abyssParsed.data.party[key];
-        return {
-          id: key,
-          value: party.value,
-          own_rate: party.own_rate,
-          use_by_own_rate: party.use_by_own_rate
-        };
-      })
+      characters: parseCharacterData(abyssParsed.data.character),
+      parties: {
+        firstHalf: parsePartyData(abyssParsed.data.party["1"]),
+        secondHalf: parsePartyData(abyssParsed.data.party["2"])
+      }
     };
 
     return sanitizedData;
---
+++ src/utils/abyssDataParser.ts
@@ -0,0 +1,32 @@
+export const parsePartyData = (partyData: IAbyssPartyData[]) => {
+  return partyData.map((party) => ({
+    characrerIds: party.id.split(","),
+    value: party.value,
+    ownRate: party.own_rate,
+    useByOwnRate: party.use_by_own_rate
+  }));
+};
+
+export const parseCharacterData = (characterData: IAbyssCharacter) => {
+  return Object.keys(characterData).map((key) => {
+    const character = characterData[key];
+    return {
+      id: key,
+      useRate: character.use_rate,
+      ownRate: character.own_rate,
+      useByOwnRate: character.use_by_own_rate,
+      weapons: character.weapons.map((weapon) => ({
+        id: weapon.id,
+        value: weapon.value
+      })),
+      artifacts: character.artifacts.map((artifact) => ({
+        set: artifact.set,
+        value: artifact.value
+      })),
+      constellations: character.constellations.map((constellation) => ({
+        id: constellation.id,
+        value: constellation.value
+      }))
+    };
+  });
+};
