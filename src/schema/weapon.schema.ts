94a0fe896e5ebfd2afabc65443c122cb7ddd0009
Nilesh Viduranga <nnviduranga@gmail.com>
2025-04-21 20:55:19 +0530

    feat: add endpoint to fetch weapons by type with validation schema

--- src/schema/weapon.schema.ts
+++ src/schema/weapon.schema.ts
@@ -7,10 +7,42 @@ const params = object({
   })
 });
 
+const weaponTypeParams = object({
+  type: string({
+    required_error: "Type is required",
+    invalid_type_error: "Type must be a string"
+  }).refine(
+    (value) => {
+      const validTypes = [
+        "WEAPON_SWORD_ONE_HAND",
+        "WEAPON_CLAYMORE",
+        "WEAPON_POLE",
+        "WEAPON_CATALYST",
+        "WEAPON_BOW"
+      ];
+      return validTypes.includes(value);
+    },
+    {
+      message:
+        "Type must be one of the following: sword, claymore, polearm, catalyst, bow"
+    }
+  )
+});
+
 const getWeaponByIdSchema = {
   params
 };
 
-type GetWeaponByIdInput = TypeOf<typeof params>;
+const getWeaponByTypeSchema = {
+  params: weaponTypeParams
+};
 
-export { getWeaponByIdSchema, type GetWeaponByIdInput };
+type GetWeaponByIdInput = TypeOf<typeof params>;
+type GetWeaponByTypeInput = TypeOf<typeof weaponTypeParams>;
+
+export {
+  getWeaponByIdSchema,
+  type GetWeaponByIdInput,
+  getWeaponByTypeSchema,
+  type GetWeaponByTypeInput
+};
